38ba403efddb6a7d25bb278d228d2e990dc5c786
Loopfelloff <abhiyanregmi@gmail.com>
2025-09-24 21:58:40 +0545

    added the actual folder

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = " "
+vim.keymap.set("n" , "<leader>cd", vim.cmd.Ex) -- n in the front indicates the normal mode, leader cd meanign if you press space bar waits for the c and d to be types simultaenuously vim.cmd.Ex is for :Ex
