827ef057fac8e873e17905b8bea389574b71078f
Loopfelloff <abhiyanregmi@gmail.com>
2025-09-26 17:58:44 +0545

    feat: added a shortcut bind to auto complete brackets

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -11,6 +11,24 @@ local function wrap_word(left, right)
         vim.api.nvim_set_current_line(new_line)
     end
 end
+
+local function auto_complete_brackets(left_bracket)
+    local right_bracket = ""
+    if left_bracket == "{" then
+        right_bracket = "}"
+    elseif left_bracket == "[" then
+        right_bracket = "]"
+    elseif left_bracket == "(" then
+        right_bracket = ")"
+    end
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local line = vim.api.nvim_get_current_line()
+    local new_line = line:sub(1, col) .. left_bracket .. right_bracket .. line:sub(col + 1)
+    vim.api.nvim_set_current_line(new_line)
+    row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+end
+
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>{", function()
     wrap_word("{", "}")
@@ -26,4 +44,13 @@ vim.keymap.set("n", "<leader>'", function()
 end, { silent = true })
 vim.keymap.set("n", '<leader>"', function()
     wrap_word('"', '"')
+end, { silent = true })
+vim.keymap.set("i", "{", function()
+    auto_complete_brackets("{")
+end, { silent = true })
+vim.keymap.set("i", "(", function()
+    auto_complete_brackets("(")
+end, { silent = true })
+vim.keymap.set("i", "[", function()
+    auto_complete_brackets("[")
 end, { silent = true })
