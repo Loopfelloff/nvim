fc806cae9bbd0691550103d04e00f2e2a6a7f63e
Loopfelloff <abhiyanregmi@gmail.com>
2025-09-29 13:07:21 +0545

    gave the ejs file html type highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,28 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-    require("nvim-treesitter.install").compilers = {"zig"}
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-        highlight = {
-        enable = true,
-        },
-        indent = {enable = true},
-        autotag = {enable = true},
-        auto_install = false,
-    })
+        require("nvim-treesitter.install").compilers = { "zig" }
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+            highlight = {
+                enable = true,
+            },
+            indent = { enable = true },
+            autotag = { enable = true },
+            auto_install = false,
+        })
+        if vim.filetype and vim.filetype.add then
+            vim.filetype.add({
+                extension = { ejs = "html" },
+            })
+        else
+            -- Fallback for older Neovim
+            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+                pattern = "*.ejs",
+                callback = function()
+                    vim.bo.filetype = "html"
+                end,
+            })
+        end
     end,
 }
